d9990616a719f66cd7293de41e6f6d891e26ea77
THREADRIPPER\camer <cameron.blazevich@gmail.com>
2023-04-10 23:48:48 -0500

    return token upon register

--- src/controllers/registerController.js
+++ src/controllers/registerController.js
@@ -8,29 +8,30 @@ const auth = require('../request-handling/middleware')
 
 
 router.post("/", async (req, res) => {
-    var errors=[]
+    var errors = []
     try {
         const { email, password } = req.body;
 
-        if (!email){
+        if (!email) {
             errors.push("Email is missing");
         }
-        if (errors.length){
-            res.status(400).json({"error":errors.join(",")});
+        if (errors.length) {
+            res.status(400).json({ "error": errors.join(",") });
             return;
         }
         let userExists = false;
-        
-        
-        var sql = "SELECT * FROM Users WHERE Email = ?"        
+        let user={};
+
+
+        var sql = "SELECT * FROM Users WHERE Email = ?"
         await db.all(sql, email, (err, result) => {
             if (err) {
-                res.status(402).json({"error":err.message});
+                res.status(402).json({ "error": err.message });
                 return;
             }
-            
-            if(result.length === 0) {                
-                
+
+            if (result.length === 0) {
+
                 var salt = bcrypt.genSaltSync(10);
 
                 var data = {
@@ -39,34 +40,45 @@ router.post("/", async (req, res) => {
                     Salt: salt,
                     DateCreated: Date('now')
                 }
-        
-                var sql ='INSERT INTO Users (Email, Password, Salt, DateCreated) VALUES (?,?,?,?)'
-                var params =[data.email, data.password, data.Salt, Date('now')]
-                var user = db.run(sql, params, function (err, innerResult) {
-                    if (err){
-                        res.status(400).json({"error": err.message})
+
+                var sql = 'INSERT INTO Users (Email, Password, Salt, DateCreated) VALUES (?,?,?,?)'
+                var params = [data.email, data.password, data.Salt, Date('now')]
+                user = db.run(sql, params, function (err, innerResult) {
+                    if (err) {
+                        res.status(400).json({ "error": err.message })
                         return;
                     }
-                  
-                });           
-            }            
+                    // * CREATE JWT TOKEN
+                    const token = jwt.sign(
+                        { user_id: user.Id, Email: user.email },
+                        process.env.TOKEN_KEY,
+                        {
+                            expiresIn: "1h", // 60s = 60 seconds - (60m = 60 minutes, 2h = 2 hours, 2d = 2 days)
+                        }
+                    );
+
+                    user.Token = token;
+
+                });
+            }
             else {
                 userExists = true;
                 // res.status(404).send("User Already Exist. Please Login");  
             }
         });
-  
+
         setTimeout(() => {
-            if(!userExists) {
-                res.status(201).json("Success");    
+            if (!userExists) {
+
+                res.status(201).json(user);
             } else {
-                res.status(201).json("Record already exists. Please login");    
-            }            
+                res.status(201).json("Record already exists. Please login");
+            }
         }, 500);
 
 
     } catch (err) {
-      console.log(err);
+        console.log(err);
     }
 })
 
