3b58824147f39879a767b2b9c1025cdc74aa1e28
THREADRIPPER\camer <cameron.blazevich@gmail.com>
2023-06-20 10:36:27 -0500

    initial gamecast stuff i think

---
+++ src/controllers/gamecastController.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const { getUserTeams } = require('../database/user-teams');
+const { getRosteredPlayers } = require('../roster/roster-service');
+const { getByYahooIds } = require('../database/player-id-lookup');
+const auth = require('../request-handling/middleware');
+const router = express.Router();
+const gamecastEventEmitter = require('../gamecast/gamecast-event-emitter');
+const logger = require('../logger/logger');
+
+let clients = [];
+let facts = [];
+
+function eventsHandler(request, response, next) {
+    const headers = {
+        'Content-Type': 'text/event-stream',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'no-cache'
+    };
+    response.writeHead(200, headers);
+
+
+
+    const data = `data: ${JSON.stringify(facts)}\n\n`; // \n\n indicates the end of an event
+
+    response.write(data);
+
+    const clientId = Date.now();
+
+    const newClient = {
+        id: clientId,
+        response
+    };
+
+    clients.push(newClient);
+
+    request.on('close', () => {
+        console.log(`${clientId} Connection closed`);
+        clients = clients.filter(client => client.id !== clientId);
+    });
+}
+
+gamecastEventEmitter.on('newGameData', (newData) => {
+    logger.debug('data caught')
+    sendEventsToAll(newData)
+})
+
+function sendEventsToAll(event) {
+    clients.forEach(client => client.response.write(`data: ${JSON.stringify(event)}\n\n`))
+}
+
+async function test(request, respsonse, next) {
+    const newFact = request.body;
+    facts.push(newFact);
+    respsonse.json(newFact)
+    return sendEventsToAll(newFact);
+}
+
+router.post('/fact', test);
+
+
+router.get('/events', eventsHandler);
+
+router.get('/allRosteredPlayers', auth, async (req, res) => {
+    const user = req.user;
+    const allTeams = await getUserTeams(user.user_id);
+
+    const rosteredPlayers = [];
+
+    for (let i = 0; i < allTeams.length; i++) {
+        const team = allTeams[i];
+        try {
+            const myPlayers = await getRosteredPlayers(team.user_id, team.league_id, team.team_id);
+            const playerIds = myPlayers.map(p => p.player_id);
+
+            const playerInfo = await getByYahooIds(playerIds);
+            const mapped = playerInfo.map(pi => {
+                const mapped = {name: pi.mlbname, yahoo_id: pi.yahooid, mlbid: pi.mlbid};
+                return mapped;
+            })
+            rosteredPlayers.push(...mapped)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    return res.status(200).json(rosteredPlayers);
+})
+
+module.exports = router;
---
+++ src/gamecast/live-game-refresher.js
@@ -0,0 +1,58 @@
+const axios =require('axios');
+const logger = require("../logger/logger");
+const gamecastEventEmitter = require('./gamecast-event-emitter');
+
+
+async function getLiveGameStats() {
+    logger.debug(`Getting live game stats...`);
+
+    const gameScheduleUrl = `https://statsapi.mlb.com/api/v1/schedule?sportId=1`;
+    const schedule = await axios.get(gameScheduleUrl);
+    
+   
+    const hasGames = schedule?.data?.dates?.[0]?.games?.length > 0;
+    if (!hasGames) {
+        logger.debug('no games returned on the schedule...')
+        return;
+    }
+
+    const inProgressGames = schedule.data.dates[0].games.filter(g => g.status.statusCode === 'I');
+    if (inProgressGames.length === 0) {
+        logger.debug('no in progress games...')
+        return;
+    }
+
+    const inProgressGameLinks = inProgressGames.map(g => g.link);
+
+    const currentHitters = [];
+
+    for(let i=0; i<inProgressGameLinks.length; i++) {
+        const gameUrl = `https://statsapi.mlb.com${inProgressGameLinks[i]}`;
+
+        const gameInfo = await axios.get(gameUrl);
+
+        const gameId = gameInfo.data.gameData.game.pk;
+        const currentPlay = gameInfo?.data?.liveData?.plays?.currentPlay;
+        
+        
+        const matchup = currentPlay?.matchup;
+        const playEvents = currentPlay?.playEvents;
+
+        const currentInfo = {gameId, matchup, playEvents}
+        currentHitters.push(currentInfo)
+
+        // decide what information I want to share with the client
+    }
+
+    if (currentHitters.length > 0) {
+        logger.debug('Emitting events')
+    gamecastEventEmitter.emit('newGameData', currentHitters);
+    }
+
+}
+
+getLiveGameStats();
+
+// setInterval(getLiveGameStats, .25 * 60 * 1000);
+
+module.exports = {getLiveGameStats}
